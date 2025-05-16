f51eae2eb0a1e1aacf33e93f04ca7a215f91815d
haseeb-iqbal <a.haseeb19@gmail.com>
2025-05-16 21:18:34 +0500

    Updated loading icon and app title

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,18 @@ import CircularProgress from "@mui/material/CircularProgress";
 export default function loading() {
   return (
     <div className="h-[80vh] w-full flex items-center justify-center">
-      <CircularProgress />
+      <CircularProgress
+        color="success"
+        sx={{
+          filter: "drop-shadow(0 0 10px rgba(0,255,0,0.5))",
+          color: "green",
+          "&.MuiCircularProgress-root": {
+            animationDuration: "0.7s",
+          },
+        }}
+        value={70}
+        size={90}
+      />
     </div>
   );
 }
